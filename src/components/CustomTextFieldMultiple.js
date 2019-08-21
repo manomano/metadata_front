5afc63ff1bba5ca1311c837d96c1841683f290bb
NAPR\mjaparidze <mjaparidze@napr.local>
2019-08-21 19:44:57 +0400

    inputs migrated to form

--- src/components/CustomTextFieldMultiple.js
+++ src/components/CustomTextFieldMultiple.js
@@ -8,6 +8,7 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
 
+
 const useStyles = makeStyles(theme => ({
   typography: {
     padding: '3%',
@@ -37,18 +38,7 @@ const useStyles = makeStyles(theme => ({
 function Customtextfield(props) {
 
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = useState(null);
 
-
-  const open = Boolean(anchorEl);
-
-  function handleClick(event) {
-    setAnchorEl(event.currentTarget);
-  }
-
-  function handleClose() {
-    setAnchorEl(null);
-  }
 
   function addElement() {
     props.addElement(props.num);
@@ -92,7 +82,7 @@ function Customtextfield(props) {
           inputProps={{style:{height:'0.6em'}}}
           id="standard-name"
           label={props.label}
-          value={props.value.map?props.value[0]:props.value}
+          //value={props.value.map?props.value[0]:props.value}
           onBlur={(e)=>{onchange(e)}}
           margin="normal"
           className={classes.textField}
@@ -100,78 +90,11 @@ function Customtextfield(props) {
           fullWidth
         />
       </Box>
-      <Box component="span" display="inline-block" css={{marginTop: 25,  padding:0}} p={1} m={1}>
-        <Button aria-describedby={props.num} variant="contained" onClick={handleClick}>
-          აღწერა/განმარტება
-        </Button>
-      </Box>
-      {props.value.map ?
-        <Box component="div" display="inline-block" css={{marginTop: 25}} p={1} m={1}>
-          <Button variant="contained" onClick={addElement} className={classes.button}>+</Button>
-        </Box> : ""}
-      <Box component="div" display="block" css={{paddingLeft: 15}}>
-        {
-          props.value.map ?
-            props.value.slice(1,props.value.length).map((row, i) =>
-
-              <Box display="flex"
-                   flexWrap="wrap" css={{height: 65, border: '0px solid green'}} key={"cont_" + i+1}>
-                <Box display="inline-block" css={{width: 550}} key={i+1}>
-                  <TextField
-                    label={props.label}
-                    value={props.value[i+1]}
-                    onChange={(e)=>{onListChange(e,i+1)}}
-                    margin="normal"
-                    className={classes.textField}
-                    variant="outlined"
-                    fullWidth
-                    required
-                  />
-                </Box>
-                <Box display="inline-block" css={{marginTop: 20, marginLeft: 30, height: 40}} key={"delete_" + i}>
-                  <Button variant="contained" className={classes.button} onClick={() => {
-                    removeElement(i+1)
-                  }}>X</Button>
-                </Box>
-              </Box>
-            ) : ''
-
-        }
 
 
-      </Box>
 
 
-      <Popover
-        id={props.num}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-      >
-        <Typography className={classes.typography}>
 
-          <label>განმარტება</label>
-          <br/>
-          <span>{props.definition}</span>
-          <br/>
-          <label>ნიმუში</label>
-          <br/>
-          <span>{props.sample}</span>
-          <br/>
-          <label>შენიშვნა</label>
-          <br/>
-          <span>{props.note}</span>
-
-        </Typography>
-      </Popover>
 
     </Box>
   );
--- src/components/form.js
+++ src/components/form.js
@@ -6,6 +6,9 @@ import CustomSelectFieldMultiple from './CustomSelectFieldMultiple'
 import ButtonAppBar from './AppBar'
 import Button from '@material-ui/core/Button';
 import produce from "immer"
+import Box from '@material-ui/core/Box'
+import TextField from '@material-ui/core/TextField';
+import Infobox from './Infobox'
 
 const styles = {
   mydiv: {
@@ -21,6 +24,12 @@ const styles = {
     background: 'red'
 
   },
+  button: {margin: '0 0 0 0'},
+  textField: {
+    marginLeft: 1,
+    marginRight: 1
+    //theme.spacing(1)
+  }
 
 }
 
@@ -37,7 +46,7 @@ class Item extends React.Component {
 
   render() {
     return <li ref={this.props.refProp} className={this.classes.mydiv}>
-      {this.props.num + ' . ' + this.props.label} {this.props.fieldType.indexOf('_REPEATABLE')>=0?'+':''}
+      {this.props.num + ' . ' + this.props.label} {this.props.fieldType.indexOf('_REPEATABLE') >= 0 ? '+' : ''}
       {this.props.children}
     </li>
   }
@@ -50,8 +59,13 @@ class Fieldstructure extends React.Component {
   constructor(props, classes) {
     super(props)
     this.classes = classes;
-    this.state = {}
-    this.state.fieldValues = this.rec(field_structure, null, {"10.4": [{"_id":15,"value":"ავტორი"}, {"_id":17,"value":"მეურვე"}], "1.3": "dada is here"});
+    this.state = {};
+    this.state.fieldValues = this.rec(field_structure, null, {
+      "10.4": [{"_id": 15, "value": "ავტორი"}, {
+        "_id": 17,
+        "value": "მეურვე"
+      }], "1.3": "dada is here"
+    });
 
     this.removeElement = this.removeElement.bind(this)
     this.addElement = this.addElement.bind(this);
@@ -59,6 +73,9 @@ class Fieldstructure extends React.Component {
     this.onListChange = this.onListChange.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.onListChangeSpecial = this.onListChangeSpecial.bind(this);
+    this.addElement = this.addElement.bind(this)
+
+    console.log(this.state.fieldValues)
   }
 
   rec = (arr, obj, ObjectValue) => {
@@ -66,21 +83,38 @@ class Fieldstructure extends React.Component {
       obj = {}
     }
     for (let i = 0; i < arr.length; i++) {
-      this[("ref_"+arr[i].num).trim()] = React.createRef()
+      this[("ref_" + arr[i].num).trim()] = React.createRef()
       if (arr[i].children && arr[i].children.length) {
         this.rec(arr[i].children, obj, ObjectValue)
       } else {
-        obj[arr[i].num] = ObjectValue[arr[i].num] || (arr[i].fieldType.indexOf('_REPEATABLE') > 0 ? [] : "")
+        arr[i].parent = arr;
+        obj[arr[i].num] = ObjectValue[arr[i].num] || (arr[i].fieldType.indexOf('_REPEATABLE') > 0 ? [""] : "")
+        //!ObjectValue[arr[i].num]?[]:((!ObjectValue[arr[i].num].map)?[ObjectValue[arr[i].num]]:ObjectValue[arr[i].num])
+          //ObjectValue[arr[i].num] || (arr[i].fieldType.indexOf('_REPEATABLE') > 0 ? [] : "")
       }
     }
 
     return obj;
   }
 
-  removeElement(num, index) {
+  removeElement(node, index) {
 
     this.setState(function (prevState) {
-      prevState.fieldValues[num].splice(index, 1)
+      let newArr = [...prevState.fieldValues[node.num]]
+
+      newArr.splice(index, 1)
+      return {
+        fieldValues: {...prevState.fieldValues, [node.num]:newArr}
+      }
+    })
+
+  }
+
+
+  addElement(node) {
+
+    this.setState(function (prevState) {
+      prevState.fieldValues[node.num].push("")
       return {
         fieldValues: prevState.fieldValues
       }
@@ -88,19 +122,7 @@ class Fieldstructure extends React.Component {
 
   }
 
-
-  addElement(num) {
-
-    this.setState(function (prevState) {
-      prevState.fieldValues[num].push("")
-      return {
-        fieldValues: prevState.fieldValues
-      }
-    })
-
-  }
-
-  onListChange(num, arr){
+  onListChange(num, arr) {
     this.setState(function (prevState) {
       prevState.fieldValues[num] = arr;
       return {
@@ -109,57 +131,128 @@ class Fieldstructure extends React.Component {
     })
   }
 
-  handleChange(num, value){
+  handleChange(num, value) {
+    const self = this;
     this.setState(produce(draft => {
       draft.fieldValues[num] = value
-    }))
+    }), ()=> {
+      console.log(num, self.state.fieldValues[num])
+    })
   }
 
 
-  onListChangeSpecial(obj){
-      this.setState(function (prevState) {
-        prevState.fieldValues[obj.num][obj.index] = obj.value;
-        return {
-          fieldValues: prevState.fieldValues
-        }
-      })
+  onListChangeSpecial(obj) {
+    this.setState(function (prevState) {
+      prevState.fieldValues[obj.num][obj.index] = obj.value;
+      return {
+        fieldValues: prevState.fieldValues
+      }
+    })
   }
 
-  selectFieldTypes = {'SELECT_FIELD':true, 'SELECT_FIELD_REPEATABLE':true, 'SELECT_FIELD_RECOMMENDED':true,'SELECT_FIELD_MULTIPLESELECTION':true}
 
-  list=(data) => {
+  selectFieldTypes = {
+    'SELECT_FIELD': true,
+    'SELECT_FIELD_REPEATABLE': true,
+    'SELECT_FIELD_RECOMMENDED': true,
+    'SELECT_FIELD_MULTIPLESELECTION': true
+  }
+
+  getField =(node)=>{
+    switch(node.fieldType){
+      case 'TEXT_FIELD':
+
+        return <Box>
+
+          <TextField
+          key={node.num}
+          inputProps={{style:{height:'0.6em'}}}
+          id="standard-name"
+          label={node.label}
+          value={this.state.fieldValues[node.num]}
+          //onBlur={(e)=>{onchange(e)}}
+          margin="normal"
+          className={this.classes.textField}
+          variant="outlined"
+          fullWidth
+        />
+
+        </Box>
+
+      case 'TEXT_FIELD_REPEATABLE':
+        return this.state.fieldValues[node.num].map((el,index)=>{
+          return <Box key={"box_"+node.num+"_"+index}>
+                    <TextField
+                      key={node.num+"_"+index}
+                      inputProps={{style:{height:'0.6em'}}}
+                      id="standard-name"
+                      label={node.label}
+                      value={this.state.fieldValues[node.num][el]}
+                      //onBlur={(e)=>{onchange(e)}}
+                      margin="normal"
+                      className={this.classes.textField}
+                      variant="outlined"
+                      fullWidth
+                    />
+
+            <Box display="inline-block" css={{marginTop: 20, marginLeft: 30, height: 40}} key={"delete_" + index}>
+              <Button variant="contained" className={this.classes.button} onClick={() => {
+                this.removeElement(node, index)
+              }}>X</Button>
+            </Box>
+
+
+
+          </Box>
+        })
+      }
+
+
+  }
+
+
+  list = (data) => {
     const children = (items) => {
       if (items) {
         return <ul className={this.classes.mydiv}>{this.list(items)}</ul>
       }
     }
 
+
     return data.map((node, index) => {
-      return <Item refProp={this["ref_"+node.num]} key={node.num} label={node.label} num={node.num} fieldType={node.fieldType}>
+      return <Item refProp={this["ref_" + node.num]} key={node.num} label={node.label} num={node.num}
+                   fieldType={node.fieldType}>
         {(node.children && node.children.length) ? children(node.children) :
           <div>
-            {this.selectFieldTypes.hasOwnProperty(node.fieldType)?
-            <CustomSelectFieldMultiple key={node.num}
-                               onListChange ={this.onListChange}
-                               label={node.label}
-                               value={this.state.fieldValues[node.num]} num={node.num}
-                               definition={node.definition}
-                               sample={node.sample}
-                               note={node.note}/>:
-              <CustomTextFieldMultiple key={node.num}
-                               addElement={this.addElement}
-                               removeElement={this.removeElement}
-                               elementsArr={this.state.fieldValues[node.num]}
-                               handleChange={this.handleChange}
-                               onListChangeSpecial={this.onListChangeSpecial}
-                               label={node.label}
-                               value={this.state.fieldValues[node.num]}
-                               num={node.num}
-                               definition={node.definition}
-                               sample={node.sample}
-                               note={node.note}/>}
+            {
+              this.getField(node)
+              /*{this.selectFieldTypes.hasOwnProperty(node.fieldType) ?
 
-            </div>}
+              <CustomSelectFieldMultiple key={node.num}
+                                         onListChange={this.onListChange}
+                                         label={node.label}
+                                         value={this.state.fieldValues[node.num]} num={node.num}
+                                         definition={node.definition}
+                                         sample={node.sample}
+                                         note={node.note}/> :
+
+              <CustomTextFieldMultiple key={node.num}
+                                       addElement={this.addElement}
+                                       removeElement={this.removeElement}
+                                       handleChange={this.handleChange}
+                                       onListChangeSpecial={this.onListChangeSpecial}
+                                       label={node.label}
+                                       value={this.state.fieldValues[node.num]}
+                                       num={node.num}
+                                       definition={node.definition}
+                                       sample={node.sample}
+                                       note={node.note}/>}*/}
+            {node.fieldType.indexOf('REPEATABLE') >= 0 && !this.selectFieldTypes.hasOwnProperty(node.fieldType)?
+              <Box component="div" display="block" css={{marginTop: 25}} p={1} m={1}>
+                <Button variant="contained" onClick={() => (this.addElement(node))}>+</Button>
+              </Box> : ""}
+
+          </div>}
 
 
       </Item>
@@ -174,13 +267,13 @@ class Fieldstructure extends React.Component {
     return (
       <div>
         {/*<Button onClick={this.scrollToMyRef}>scrollTo</Button>*/}
-<ButtonAppBar/>
-<ul>
-{this.list(field_structure)}
-</ul>
-</div>)
+        <ButtonAppBar/>
+        <ul>
+          {this.list(field_structure)}
+        </ul>
+      </div>)
 
-}
+  }
 
 }
 
