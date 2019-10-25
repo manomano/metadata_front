f8a6f878feea5e3627a21fd890d36c8c923e2cac
NAPR\mjaparidze <mjaparidze@napr.local>
2019-10-25 20:16:46 +0400

    warning -  two children is with the same key - corrected

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -5,13 +5,13 @@ export default function Inputs(props) {
 
     const {num, children, ind} = props;
     const colCount = Math.floor(12/children.length);
-
+    const key_part = num + (props.hasOwnProperty("ind") && typeof(props.ind)!=='undefined'?"_"+ind:"");
     const childElements = children.map((child, index)=>
 
-      <div key={"div_"+props.hasOwnProperty("ind") && typeof(props.ind)!=='undefined'?ind+"_":""+child.num} className={"col-"+colCount}>
-        <Input key={"key_"+props.hasOwnProperty("ind") && typeof(props.ind)!=='undefined'?ind+"_":""+child.num} ind={ind} {...child}  />
+      <div key={"div_"+key_part+"_"+child.num} className={"col-"+colCount}>
+        <Input key={"key_"+key_part +"_"+child.num} ind={ind} {...child}  />
       </div>
     );
 
-    return (<div className={"row"} key={"inputs_"+props.hasOwnProperty("ind") && typeof(props.ind)!=='undefined'?ind+"_":""+num}>{childElements}</div>);
+    return (<div className={"row"} key={"inputs_"+key_part}>{childElements}</div>);
 }
